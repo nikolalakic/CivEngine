1fa1f00f761b90bdea3433ba98d7c61e2dece34f
Nikola Lakic <nlakic@pm.me>
2024-11-19 00:06:49 +0100

    Apdejtovana skripta
    Nosivosti betonskog preseka se računa u Karakteristike_preseka skripti

--- Scripts/Smicanje.py
+++ Scripts/Smicanje.py
@@ -1,39 +1,13 @@
-from Karakteristike_materijala import BetonIArmatura
 import math
+from Karakteristike_preseka import BetonIArmatura
 
 
 class Smicanje(BetonIArmatura):
     def __init__(self):
         super().__init__()
         self.VEd = abs(float(input('Unesi silu smicanja VEd [kN]: ')))
-        self.m = int(input('Ukupna sečnost preseka koji se kontroliše m [-]: '))
-        b = float(input('Unesi sirinu preseka b [cm]: '))
-        self.b = b/100  # [m]
-        h = float(input('Unesi visinu preseka h [cm]: '))
-        self.h = h/100  # [m]
-        d1 = float(input('Unesi rastojanje zategnute armature od zategnute ivice betona d1 [cm]: '))
-        self.d1 = d1/100  # [m]
-        Asl = float(input('Unesi povrsinu poduzne armature od savijanja u preseku ili rebru Asl [cm^2]: '))
-        self.Asl = Asl/math.pow(10, 4)  # [m^2]
-        self.d = self.h - self.d1  # [m]
-
-    def minimalna_nosivost_betona_na_smicanje(self):
-        k1 = 0.15  # nacionalni prilog
-        ro_1 = self.Asl/(self.b * self.d)
-        sigma_cp = 0  # proracun vazi za nepritisnut betonski presek
-        CRd_c = 0.18/self.gama_c
-        k = 1 + math.sqrt(200/(self.d*1000))
-        VRd_c_1 = ((CRd_c * k * math.pow((100 * ro_1 * self.fck), 1/3) + k1 * sigma_cp) * self.b * self.d *
-                   1000)  # [kN]
-        Vmin = 0.035 * math.pow(k, 3/2) * math.pow(self.fck, 1/2) * self.b * self.d * 1000  # [kN]
-        VRd_c = max(VRd_c_1, Vmin)
-        return VRd_c
-
-    def maksimalna_nosivost_betona_na_smicanje(self):
-        fcd = self.fcd()
-        VRd_max = (self.sin_alfa_cw * self.ni_1 * fcd * self.b * 0.9 * self.d *
-                   (self.cot_teta/(1 + math.pow(self.cot_teta, 2)))) * 1000
-        return VRd_max
+        self.Asl = self.poduzna_armatura()
+        self.mv = self.secnost()
 
     def izabrana_povrsina_armature_za_smicanje(self):  # uzeti su samo fi 8, 10, i 12 u obzir
         fi_lista_povrsina = self.fi_lista_povrsina() / math.pow(10, 4)  # m^2
@@ -50,7 +24,7 @@ class Smicanje(BetonIArmatura):
         ro_w_min = 0.08 * math.sqrt(self.fck) / self.fyk
         sr_min_lista = []
         for fi_povrsina in fi_lista_povrsina:
-            sr_min = self.m * fi_povrsina / (ro_w_min * self.b)  # [m]
+            sr_min = self.mv * fi_povrsina / (ro_w_min * self.b)  # [m]
             sr_min_lista.append(sr_min)
         return sr_min_lista
 
@@ -58,7 +32,7 @@ class Smicanje(BetonIArmatura):
         fi_lista_povrsina = self.izabrana_povrsina_armature_za_smicanje()
         s_rac_lista = []
         for fi_povrsina in fi_lista_povrsina:
-            s_rac = self.m * fi_povrsina / self.VEd * 0.9 * self.d * self.fyd * 1000 * self.cot_teta  # [m]
+            s_rac = self.mv * fi_povrsina / self.VEd * 0.9 * self.d * self.fyd * 1000 * self.cot_teta  # [m]
             s_rac_lista.append(s_rac)
         return s_rac_lista
 
@@ -103,7 +77,7 @@ class Smicanje(BetonIArmatura):
         delta_As1 = delta_Ftd/(self.fyd * 1000)  # [m^2]
         return delta_As1
 
-    def rezultat_smicanje(self):
+    def rezultat(self):
         delta_As1 = self.dodatna_zategnuta_armatura()
         st_max = self.maksimalno_poprecno_rastojanje_nozica_armature()
         sr_max = self.maksimalno_poduzno_rastojanje_armature()
@@ -116,29 +90,31 @@ class Smicanje(BetonIArmatura):
         if VRd_c < self.VEd <= VRd_max:
             print('--------------------')
             for s, fi, sr_min in zip(s_rac_lista, fi_lista, sr_min_lista):
-                odgovor = (f'Potrebno rastojanje armature s = {round(min(s, sr_min, sr_max), 4)*100}'
+                odgovor = (f'Potrebno rastojanje armature: s = {round(min(s, sr_min, sr_max), 4)*100}'
                            f' [cm] za prečnik uzengije \u03C6 = {fi}')
                 rezultat += odgovor + '\n'
-            print(f'Sečnost m = {self.m}')
-            print(f'Dodatna zategnuta armatura \u0394As1 =  {round(delta_As1, 4) * math.pow(10,4)} [cm^2]')
-            print(f'Maksimalno podužno rastojanje armature sr_max = {round(sr_max, 4) * 100} [cm]')
-            print(f'Maksimalno poprečno rastojanje armature st_max = {round(st_max, 4) * 100} [cm]\n')
+            print(f'Sečnost m = {self.mv}')
+            print(f'Dodatna zategnuta armatura: \u0394As1 =  {round(delta_As1, 4) * math.pow(10,4)} [cm^2]')
+            print(f'Maksimalno podužno rastojanje armature: sr_max = {round(sr_max, 4) * 100} [cm]')
+            print(f'Maksimalno poprečno rastojanje armature: st_max = {round(st_max, 4) * 100} [cm]\n')
+            print(f'Maksimalna nosivost pritisnute betonske dijagonale: VRd,max = {round(VRd_max, 2)} kN')
         elif self.VEd <= VRd_c:
             print('--------------------')
             print('Minimalni procenat armiranja!\n')
             for s, fi, sr_min in zip(s_rac_lista, fi_lista, sr_min_lista):
-                odgovor = (f'Potrebno rastojanje armature s = {round(min(s, sr_min, sr_max), 4)*100} [cm] '
-                           f'za prečnik uzengije \u03C6 = {fi}')
+                odgovor = (f'Potrebno rastojanje armature: s = {round(min(s, sr_min, sr_max), 4)*100} [cm] '
+                           f'za prečnik uzengije: \u03C6 = {fi}')
                 rezultat += odgovor + '\n'
-            print(f'Sečnost m = {self.m}')
-            print(f'Dodatna zategnuta armatura \u0394As1 =  {round(delta_As1, 4) * math.pow(10, 4)} [cm^2]')
-            print(f'Maksimalno podužno rastojanje armature sr_max = {round(sr_max, 4) * 100} [cm]')
-            print(f'Maksimalno poprečno rastojanje armature st_max = {round(st_max, 4) * 100} [cm]\n')
+            print(f'Sečnost: m = {self.mv}')
+            print(f'Dodatna zategnuta armatura: \u0394As1 =  {round(delta_As1, 4) * math.pow(10, 4)} [cm^2]')
+            print(f'Maksimalno podužno rastojanje armature: sr_max = {round(sr_max, 4) * 100} [cm]')
+            print(f'Maksimalno poprečno rastojanje armature: st_max = {round(st_max, 4) * 100} [cm]\n')
+            print(f'Maksimalna nosivost pritisnute betonske dijagonale: VRd,max = {round(VRd_max, 2)} kN')
 
         else:
             print('--------------------')
             rezultat = 'Prekoračena maksimalna nosivost pritisnute betonske dijagonale na smicanje!\n'
-            print(f'VEd = {self.VEd} [kN] > {VRd_max} [kN] = VRd_max')
+            print(f'VEd = {self.VEd} [kN] > {round(VRd_max, 2)} [kN] = VRd_max')
         return print(rezultat)
 
 
